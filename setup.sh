d18febf65db865f4b5881f3c808f055068db9d6b
mmccollum2 <morgan@mmccollum.com>
2018-06-04 12:01:06 -0600

    Update setup.sh
    Added commenting, static ip config, ssh key fixing, added raspi-config back after purge

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Downsize and prep from standard Raspbian Image
 
-sudo rpi-update && sudo apt -y update && sudo apt -y upgrade && sudo apt -y install vim
+sudo rpi-update && sudo apt -y update && sudo apt -y upgrade
 
 sudo apt-get purge --auto-remove scratch debian-reference-en dillo idle3 python3-tk idle python-pygame python-tk lightdm gnome-themes-standard gnome-icon-theme raspberrypi-artwork gvfs-backends gvfs-fuse desktop-base lxpolkit netsurf-gtk zenity xdg-utils mupdf gtk2-engines alsa-utils  lxde lxtask menu-xdg gksu midori xserver-xorg xinit xserver-xorg-video-fbdev libraspberrypi-dev libraspberrypi-doc dbus-x11 libx11-6 libx11-data libx11-xcb1 x11-common x11-utils lxde-icon-theme gconf-service gconf2-common xserver* ^x11 ^libx ^lx samba* -y
+
+sudo apt -y install vim raspi-config
 
 sudo apt-get clean -y && sudo apt-get autoremove -y
 
@@ -23,9 +25,18 @@ if [ $? -eq 0 ]; then
    (umask 077; mkdir -p /home/pi/.ssh; touch /home/pi/.ssh/authorized_keys)
    chown -R $(id -u pi):$(id -g pi) /home/pi/.ssh
    curl -sSL https://github.com/${github_user}.keys >> /home/pi/.ssh/authorized_keys
-   sed -i 's|[#]*PasswordAuthentication yes|PasswordAuthentication no|g' /etc/ssh/sshd_config
    echo "Keys installed from gitub.com"
  else
    echo "Won't install ssh keys, github.com couldn't be reached."
  fi
  /etc/init.d/ssh reload
+
+#Run this later when you're sure your ssh key access works and you disable password auth
+#sed -i 's|[#]*PasswordAuthentication yes|PasswordAuthentication no|g' /etc/ssh/sshd_config
+
+#One liner for static ip config
+#sudo sed -i '$ a\interface eth0\nstatic ip_address=0.0.0.0/24\nstatic routers=0.0.0.1\nstatic domain_name_servers=208.67.222.222 1.1.1.1\n' /etc/dhcpcd.conf
+#this will disable dhcp on reboot
+#sudo update-rc.d dhcpcd disable
+#sudo reboot
+
